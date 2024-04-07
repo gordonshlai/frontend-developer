175a5c770f848f3be6577c76edd0a3bf53943619
Gordon Lai <gordonshlai@yahoo.com.hk>
2024-04-07 20:57:25 +0100

    add navBar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import styles from "./App.module.scss";
 
 function App() {
-  return <div className="background"></div>;
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navBar}>navBar</nav>
+      <main className={styles.main}>main</main>
+    </div>
+  );
 }
 
 export default App;
