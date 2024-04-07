39bb4712625b5cedc20d680bdc3e78fbdf1f7c4c
Gordon Lai <gordonshlai@yahoo.com.hk>
2024-04-07 22:35:19 +0100

    add navBar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import styles from "./App.module.scss";
 import SortElement from "./components/SortElement";
+import { MdSortByAlpha } from "react-icons/md";
+import { RiMoneyPoundCircleFill } from "react-icons/ri";
+import { FaStar } from "react-icons/fa6";
+
+const options = [
+  { text: "alphabetically", icon: <MdSortByAlpha /> },
+  { text: "price", icon: <RiMoneyPoundCircleFill /> },
+  { text: "star rating", icon: <FaStar /> },
+];
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className={styles.container}>
       <nav className={styles.navBar}>
-        <SortElement />
-        <SortElement />
-        <SortElement />
+        {options?.map((option, index) => (
+          <SortElement
+            option={option}
+            key={option?.text + index}
+            selected={selected}
+            onClick={() => setSelected(option)}
+          />
+        ))}
       </nav>
       <main className={styles.main}>main</main>
     </div>
--- src/components/SortElement.jsx
+++ src/components/SortElement.jsx
@@ -1,11 +1,17 @@
-import { MdSortByAlpha } from "react-icons/md";
 import styles from "./SortElement.module.scss";
 
-const SortElement = () => {
+const SortElement = ({ option, selected, onClick }) => {
+  const active = selected === option;
+
   return (
-    <div className={styles.container}>
-      <p>sort alphabetically</p>
-      <MdSortByAlpha size={"1.3rem"} />
+    <div className={`${styles.container} ${active ? styles.active : styles.inactive}`} onClick={onClick}>
+      <div>
+        <p className={styles.text}>sort</p>
+        <p className={styles.text}>{option?.text !== "alphabetically" && " by"}</p>
+        <p className={`${styles.text} ${styles.bold}`}>{` ${option?.text}`}</p>
+      </div>
+
+      <div className={`${styles?.icon} ${!active && styles.inactiveIcon}`}>{option?.icon}</div>
     </div>
   );
 };
