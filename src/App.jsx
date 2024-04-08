9d72b15ae81ca0fbbc1ead7d02e1530a7618af95
Gordon Lai <gordonshlai@yahoo.com.hk>
2024-04-08 02:01:13 +0100

    add deals skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,66 @@
 import { useState } from "react";
-import styles from "./App.module.scss";
-import SortElement from "./components/SortElement";
+import { FaStar } from "react-icons/fa6";
 import { MdSortByAlpha } from "react-icons/md";
 import { RiMoneyPoundCircleFill } from "react-icons/ri";
-import { FaStar } from "react-icons/fa6";
+import styles from "./App.module.scss";
+import hotel1 from "./assets/hotel-image-1.png";
+import hotel2 from "./assets/hotel-image-2.png";
+import hotel3 from "./assets/hotel-image-3.png";
+import Deal from "./components/Deal";
+import SortElement from "./components/SortElement";
 
 const options = [
   { text: "alphabetically", icon: (color) => <MdSortByAlpha size={"1.3rem"} color={color} /> },
   { text: "price", icon: (color) => <RiMoneyPoundCircleFill size={"1.3rem"} color={color} /> },
   { text: "star rating", icon: (color) => <FaStar size={"1.3rem"} color={color} /> },
+];
+
+const deals = [
+  {
+    img: hotel1,
+    title: "Iberostar Grand Salome",
+    address: "Costa Adeje, Tenerife",
+    stars: 5,
+    adults: 2,
+    children: 2,
+    infants: 1,
+    date: "3rd July 2019",
+    duration: "7 days",
+    departingFrom: "East Midlands",
+    price: "1,136.50",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
+  {
+    img: hotel2,
+    title: "Aguamarina Golf Hotel",
+    address: "Costa Adeje, Tenerife",
+    stars: 4,
+    adults: 2,
+    children: 1,
+    infants: 0,
+    date: "27th May 2019",
+    duration: "7 days",
+    departingFrom: "Liverpool",
+    price: "696.80",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
+  {
+    img: hotel3,
+    title: "Las Piramides Resort",
+    address: "Costa Adeje, Tenerife",
+    stars: 3,
+    adults: 2,
+    children: 2,
+    infants: 0,
+    date: "3rd July 2019",
+    duration: "7 days",
+    departingFrom: "Manchester",
+    price: "499.99",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
 ];
 
 function App() {
@@ -26,7 +78,11 @@ function App() {
           />
         ))}
       </nav>
-      <main className={styles.main}>main</main>
+      <main>
+        {deals?.map((deal, index) => (
+          <Deal deal={deal} key={index} />
+        ))}
+      </main>
     </div>
   );
 }
---
+++ src/components/Deal.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { FaChevronDown, FaChevronRight } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+import styles from "./Deal.module.scss";
+import Text from "./Text";
+
+const Deal = ({ deal }) => {
+  const { img, title, address, stars, adults, children, infants, date, duration, departingFrom, price, overview } =
+    deal;
+
+  const [isOverviewVisible, setIsOverviewVisible] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.description}>
+        <img src={img} />
+        <div>
+          <header>{title}</header>
+
+          <Text>{address}</Text>
+
+          <div>
+            {Array(stars)
+              .fill(true)
+              .map((_, i) => (
+                <FaStar color={"#fedc07"} key={i} />
+              ))}
+          </div>
+
+          <div>
+            {!!adults && (
+              <>
+                <Text bold>{adults}</Text>
+                <Text>{` adult${adults > 1 ? "s" : ""}`}</Text>
+              </>
+            )}
+            {!!children && (
+              <>
+                <Text>{", "}</Text>
+                <Text bold>{children}</Text>
+                <Text>{` child${children > 1 ? "ren" : ""}`}</Text>
+              </>
+            )}
+            {!!infants && (
+              <>
+                <Text>{` & `}</Text>
+                <Text bold>{infants}</Text>
+                <Text>{` infant${infants > 1 ? "s" : ""}`}</Text>
+              </>
+            )}
+          </div>
+
+          <div>
+            <Text bold>{date}</Text>
+            <Text>{" for "}</Text>
+            <Text bold>{duration}</Text>
+          </div>
+
+          <div>
+            <Text>{"departing from "}</Text>
+            <Text bold>{departingFrom}</Text>
+          </div>
+
+          <div className={styles.button}>
+            <p>Book now</p>
+            <p>{`£ ${price}`}</p>
+          </div>
+        </div>
+
+        <div className={styles.accordion} onClick={() => setIsOverviewVisible(!isOverviewVisible)}>
+          <Text bold>{`Read ${isOverviewVisible ? "less" : "more"}`}</Text>
+          <Text>&nbsp;about this hotel</Text>
+          {isOverviewVisible ? <FaChevronDown size={"1.3rem"} /> : <FaChevronRight size={"1.3rem"} />}
+        </div>
+      </section>
+
+      {isOverviewVisible && (
+        <article>
+          <header>Overview</header>
+          <Text>{overview}</Text>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default Deal;
