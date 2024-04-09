317cf71073342cc7a96603170d5106bb1ba42fe5
Gordon Lai <gordonshlai@yahoo.com.hk>
2024-04-09 01:11:11 +0100

    add sorting algorithms

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,47 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { FaStar } from "react-icons/fa6";
 import { MdSortByAlpha } from "react-icons/md";
 import { RiMoneyPoundCircleFill } from "react-icons/ri";
 import styles from "./App.module.scss";
-import hotel1 from "./assets/hotel-image-1.png";
-import hotel2 from "./assets/hotel-image-2.png";
-import hotel3 from "./assets/hotel-image-3.png";
 import Deal from "./components/Deal/Deal";
 import SortElement from "./components/SortElement";
+import deals from "./data/deals";
 
 const options = [
-  { text: "alphabetically", icon: (color) => <MdSortByAlpha size={"1.3rem"} color={color} /> },
-  { text: "price", icon: (color) => <RiMoneyPoundCircleFill size={"1.3rem"} color={color} /> },
-  { text: "star rating", icon: (color) => <FaStar size={"1.3rem"} color={color} /> },
-];
-
-const deals = [
   {
-    img: hotel1,
-    title: "Iberostar Grand Salome",
-    address: "Costa Adeje, Tenerife",
-    stars: 5,
-    adults: 2,
-    child: 2,
-    infants: 1,
-    date: "3rd July 2019",
-    duration: "7 days",
-    departingFrom: "East Midlands",
-    price: "1,136.50",
-    overview:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+    text: "alphabetically",
+    icon: (color) => <MdSortByAlpha size={"1.3rem"} color={color} />,
+    sortingFunction: (a, b) => a.title.localeCompare(b.title),
   },
   {
-    img: hotel2,
-    title: "Aguamarina Golf Hotel",
-    address: "Costa Adeje, Tenerife",
-    stars: 4,
-    adults: 2,
-    child: 1,
-    infants: 0,
-    date: "27th May 2019",
-    duration: "7 days",
-    departingFrom: "Liverpool",
-    price: "696.80",
-    overview:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+    text: "price",
+    icon: (color) => <RiMoneyPoundCircleFill size={"1.3rem"} color={color} />,
+    sortingFunction: (a, b) => parseFloat(a.price.replace(",", "")) - parseFloat(b.price.replace(",", "")),
   },
   {
-    img: hotel3,
-    title: "Las Piramides Resort",
-    address: "Costa Adeje, Tenerife",
-    stars: 3,
-    adults: 2,
-    child: 2,
-    infants: 0,
-    date: "3rd July 2019",
-    duration: "7 days",
-    departingFrom: "Manchester",
-    price: "499.99",
-    overview:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+    text: "star rating",
+    icon: (color) => <FaStar size={"1.3rem"} color={color} />,
+    sortingFunction: (a, b) => b.stars - a.stars,
   },
 ];
 
 function App() {
   const [selected, setSelected] = useState(options[0]);
+  const sortedDeals = [...deals].sort(selected.sortingFunction);
 
   return (
     <div className={styles.container}>
-      <nav>
+      <section>
         {options?.map((option, index) => (
-          <>
+          <Fragment key={option?.text + index}>
             {index !== 0 && <div className={styles.divider} />}
-            <SortElement
-              option={option}
-              key={option?.text + index}
-              selected={selected}
-              onClick={() => setSelected(option)}
-            />
-          </>
+            <SortElement option={option} selected={selected} onClick={() => setSelected(option)} />
+          </Fragment>
         ))}
-      </nav>
+      </section>
       <main>
-        {deals?.map((deal, index) => (
-          <Deal deal={deal} key={index} />
+        {sortedDeals?.map((deal, index) => (
+          <Deal deal={deal} key={deal?.title + index} />
         ))}
       </main>
     </div>
---
+++ src/data/deals.js
@@ -0,0 +1,51 @@
+import hotel1 from "../assets/hotel-image-1.png";
+import hotel2 from "../assets/hotel-image-2.png";
+import hotel3 from "../assets/hotel-image-3.png";
+
+export default [
+  {
+    img: hotel1,
+    title: "Iberostar Grand Salome",
+    address: "Costa Adeje, Tenerife",
+    stars: 5,
+    adults: 2,
+    child: 2,
+    infants: 1,
+    date: "3rd July 2019",
+    duration: "7 days",
+    departingFrom: "East Midlands",
+    price: "1,136.50",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
+  {
+    img: hotel2,
+    title: "Aguamarina Golf Hotel",
+    address: "Costa Adeje, Tenerife",
+    stars: 4,
+    adults: 2,
+    child: 1,
+    infants: 0,
+    date: "27th May 2019",
+    duration: "7 days",
+    departingFrom: "Liverpool",
+    price: "696.80",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
+  {
+    img: hotel3,
+    title: "Las Piramides Resort",
+    address: "Costa Adeje, Tenerife",
+    stars: 3,
+    adults: 2,
+    child: 2,
+    infants: 0,
+    date: "3rd July 2019",
+    duration: "7 days",
+    departingFrom: "Manchester",
+    price: "499.99",
+    overview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repellat id aut, modi velit amet sint ea voluptate dolorum, molestiae vel accusamus commodi esse laborum. Eligendi nisi pariatur ut voluptate nam ducimus similique eaque, ratione vel. Corrupti beatae est sit fugit at labore esse blanditiis tempora mollitia. Nobis, unde quibusdam.",
+  },
+];
